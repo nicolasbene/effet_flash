202abf1f02b0976d0afacc14e0df699dfe9ff438
Nicolas Benoit <nicolas.benoit.pro@gmail.com>
2021-12-15 12:42:56 +0100

    effet_ok

---
+++ app.js
@@ -0,0 +1,16 @@
+// function flash(){
+//   $('.container')
+//     // .fadeOut(1)
+//     // .show()  //show the hidden div
+//     .css({'opacity': 0})
+//     .animate({opacity: 1}, 2000);
+// }
+
+
+
+document.addEventListener("DOMContentLoaded",function(){
+  $('.image').fadeIn(1500);
+  $('.texte').fadeIn(1500);
+  // flash();
+
+});
